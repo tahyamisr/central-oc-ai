33439d9bcf310a8f1be7c70058f7c98299e4cdb5
اتحاد طلاب تحيا مصر <tahyamisr.online@gmail.com>
2025-09-21 00:15:16 +0000

    اجعل الشعار فى اقصي اليمين (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,9 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-muted/50">
       <header className="sticky top-0 z-50 w-full bg-accent text-accent-foreground border-b">
         <div className="container flex h-16 items-center justify-between px-4 md:px-6">
-          <div className="flex items-center gap-4">
-            <span className="font-bold text-xl">
-              لجنة التنظيم والمراسم المركزية
-            </span>
-          </div>
+          <span className="font-bold text-xl">
+            لجنة التنظيم والمراسم المركزية
+          </span>
           <Image
             src="https://www.dropbox.com/scl/fi/2ypsrr8n9lj9daty5sq5x/Central-OC.png?rlkey=9ujc2o9sj96vfrgofbqllt6ni&raw=1"
             alt="شعار اتحاد طلاب تحيا مصر"
